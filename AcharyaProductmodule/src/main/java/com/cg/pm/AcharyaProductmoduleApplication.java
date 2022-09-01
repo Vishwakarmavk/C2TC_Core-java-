1c15f1fdf258008c11dd5d57b204981466dc8b41
acer <acer@Z1-471>
2022-09-01 12:26:59 +0530

    first commit

---
+++ AcharyaProductmodule/src/main/java/com/cg/pm/AcharyaProductmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.cg.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcharyaProductmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcharyaProductmoduleApplication.class, args);
+	}
+
+}
